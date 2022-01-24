a472b08c61e5a445d3bb3a76de3b1a555120f8f8
claranceliberi <liberintwari@gmail.com>
2022-01-24 19:32:41 +0200

    feat: config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#502274",
+        secondary: "#E5E5E5",
+      },
+    },
   },
   plugins: [],
 };
