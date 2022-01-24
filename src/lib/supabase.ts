a41e82bcf4849a14d28a0658b5c47b99a1230141
claranceliberi <liberintwari@gmail.com>
2022-01-24 15:01:59 +0200

    feat: superbase config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.VUE_APP_SUPABASE_URL as string,
-  process.env.VUE_APP_SUPABASE_KEY as string,
-);
+const SUPABASE_URL = "https://tofusbgfjuqfuwzawljq.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQzMDAxOTE4LCJleHAiOjE5NTg1Nzc5MTh9.xqXs4kuTGfJK6gQVhiVz01lVRCiFmbUkkdd4L3AxoMw";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
