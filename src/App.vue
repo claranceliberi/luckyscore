c5ea0a8df4707d2d7b8facca9e041bc0e1cdc10c
claranceliberi <liberintwari@gmail.com>
2022-01-24 15:42:53 +0200

    chore: tailwind config

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,6 @@
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="bg-red-900">test tailwind</div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
-
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
