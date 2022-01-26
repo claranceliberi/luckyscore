1f2e4243d1b191200a679786559f8d8336955740
oreste-abizera <oresteabizera11@gmail.com>
2022-01-26 21:46:10 +0200

    chore: live table ui

---
+++ src/components/MatchView/LiveTable.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+  interface Team {
+    name: string;
+    played: number;
+    wins: number;
+    draws: number;
+    losses: number;
+    goalsForward: number;
+    goalsAgainst: number;
+    goalDifference: number;
+    points: number;
+  }
+  const teams: Array<Team> = [
+    {
+      name: "Year Three",
+      played: 3,
+      wins: 1,
+      draws: 1,
+      losses: 1,
+      goalsForward: 1,
+      goalsAgainst: 1,
+      goalDifference: 1,
+      points: 4,
+    },
+    {
+      name: "Welding L2",
+      played: 3,
+      wins: 1,
+      draws: 1,
+      losses: 1,
+      goalsForward: 1,
+      goalsAgainst: 1,
+      goalDifference: 1,
+      points: 4,
+    },
+  ];
+</script>
+
+<template>
+  <div class="bg-white shadow-sm p-4">
+    <h1 class="font-black mt-4 mb-8">Live Table</h1>
+    <div>
+      <div class="flex md:pr-8">
+        <div class="flex-1 flex"></div>
+        <div class="flex gap-4">
+          <div class="w-4 md:w-6 text-center">P</div>
+          <div class="w-4 md:w-6 text-center">W</div>
+          <div class="w-4 md:w-6 text-center">D</div>
+          <div class="w-4 md:w-6 text-center">L</div>
+          <div class="w-4 md:w-6 text-center">GF</div>
+          <div class="w-4 md:w-6 text-center">GA</div>
+          <div class="w-4 md:w-6 text-center">GD</div>
+          <div class="w-4 md:w-6 text-center">PTS</div>
+        </div>
+      </div>
+      <div
+        v-for="(team, index) in teams"
+        :key="team.name"
+        class="my-4 flex md:pr-8"
+      >
+        <div class="flex-1 flex">
+          <p class="text-black mx-2">{{ index + 1 }}</p>
+          <p class="text-black mx-2">{{ team.name }}</p>
+        </div>
+        <div class="flex gap-4">
+          <div class="w-4 md:w-6 text-center">
+            {{ team.played }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.wins }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.draws }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.losses }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.goalsForward }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.goalsAgainst }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.goalDifference }}
+          </div>
+          <div class="w-4 md:w-6 text-center">
+            {{ team.points }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/public/home/MatchView.vue
+++ src/views/public/home/MatchView.vue
@@ -1,41 +1,45 @@
 <template>
-  <div class="match__container mb-4">
-    <h1 className="header text-2xl font-bold mb-5 mt-10">Line-Ups</h1>
-    <div class="flex flex-col md:flex-row gap-8 md:gap-0 mt-4">
-      <div class="w-full md:w-1/2">
-        <div class="teams__header flex">
-          <div
-            class="active cursor-pointer text-primary w-1/2 pb-4 border-b-2 border-primary"
-          >
-            <p class="text-center">Welding L3</p>
+  <div>
+    <div class="match__container mb-4">
+      <h1 className="header text-2xl font-bold mb-5 mt-10">Line-Ups</h1>
+      <div class="flex flex-col md:flex-row gap-8 md:gap-0 mt-4">
+        <div class="w-full md:w-1/2">
+          <div class="teams__header flex">
+            <div
+              class="active cursor-pointer text-primary w-1/2 pb-4 border-b-2 border-primary"
+            >
+              <p class="text-center">Welding L3</p>
+            </div>
+            <div
+              class="w-1/2 cursor-pointer pb-4 text-appgrey border-b-2 border-gray-200"
+            >
+              <p>Culinary L2</p>
+            </div>
           </div>
           <div
-            class="w-1/2 cursor-pointer pb-4 text-appgrey border-b-2 border-gray-200"
+            class="forms flex justify-around items-center flex-wrap gap-5 mt-4"
           >
-            <p>Culinary L2</p>
+            <FormationCard />
           </div>
         </div>
         <div
-          class="forms flex justify-around items-center flex-wrap gap-5 mt-4"
+          class="w-full md:w-1/2 flex flex-col md:flex-row justify-center gap-8 md:gap-0 pt-0 md:pt-20"
         >
-          <FormationCard />
-        </div>
-      </div>
-      <div
-        class="w-full md:w-1/2 flex flex-col md:flex-row justify-center gap-8 md:gap-0 pt-0 md:pt-20"
-      >
-        <div class="w-full md:w-4/5">
-          <!-- <MatchInfo></MatchInfo>
+          <div class="w-full md:w-4/5">
+            <!-- <MatchInfo></MatchInfo>
           <MatchPrediction></MatchPrediction> -->
-          <MatchStats></MatchStats>
+            <MatchStats></MatchStats>
+          </div>
         </div>
       </div>
     </div>
+    <LiveTable></LiveTable>
   </div>
 </template>
 
 <script setup lang="" ts>
   import MatchInfo from "@/components/MatchView/MatchInfo.vue";
+  import LiveTable from "@/components/MatchView/LiveTable.vue";
   import MatchStats from "@/components/MatchView/MatchStats.vue";
   import MatchPrediction from "@/components/MatchView/MatchPrediction.vue";
   import FormationCard from "@/components/formations/FormationCard.vue";
