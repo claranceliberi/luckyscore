963f23e4143ee670e19a92cd4966440fe1e98225
claranceliberi <liberintwari@gmail.com>
2022-01-27 08:23:51 +0200

    feat: not found impelemntation

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,8 @@ import Auth from "@/views/auth/AuthView.vue";
 import SignIn from "~/views/auth/SigninView.vue";
 import SignUp from "@/views/auth/SignupView.vue";
 
+import NotFound from "@/views/NotFoundView.vue";
+
 const routes = [
   {
     path: "/dashboard",
@@ -69,6 +71,7 @@ const routes = [
       { path: "/signup", name: "SignUp", component: SignUp },
     ],
   },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
 const router = createRouter({
