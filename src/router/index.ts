2b22dcdc35fe629292fb1ca1ee2e62165aa8a813
claranceliberi <liberintwari@gmail.com>
2022-02-02 12:05:30 +0200

    feat: hide menu on results

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,21 +6,21 @@ import TeamDetailsView from "@/views/dashboard/TeamDetailsView.vue";
 import NewTeamView from "@/views/NewTeamView.vue";
 import NotFound from "@/views/NotFoundView.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import SignIn from "~/views/auth/SigninView.vue";
-import IndexDashboard from "~/views/dashboard/IndexView.vue";
-import MatchesView from "~/views/dashboard/MatchesView.vue";
-import NewMatch from "~/views/dashboard/NewMatch.vue";
-import MatchDetailsView from "~/views/dashboard/MatchDetailsView.vue";
+import SignIn from "@/views/auth/SigninView.vue";
+import IndexDashboard from "@/views/dashboard/IndexView.vue";
+import MatchesView from "@/views/dashboard/MatchesView.vue";
+import NewMatch from "@/views/dashboard/NewMatch.vue";
+import MatchDetailsView from "@/views/dashboard/MatchDetailsView.vue";
 import FormationsView from "@/views/dashboard/AddFormationsView.vue";
-import AdminMatchFixturesView from "~/views/dashboard/AdminMatchFixturesView.vue";
-import DashboardTeams from "~/views/dashboard/TeamsView.vue";
-import Dashboard from "~/views/DashboardView.vue";
-import Fixtures from "~/views/public/home/FixturesView.vue";
-import MatchView from "~/views/public/home/MatchView.vue";
-import Players from "~/views/public/home/PlayersView.vue";
-import Stats from "~/views/public/home/StatsView.vue";
-import Table from "~/views/public/home/TableView.vue";
-import Home from "~/views/public/HomeView.vue";
+import AdminMatchFixturesView from "@/views/dashboard/AdminMatchFixturesView.vue";
+import DashboardTeams from "@/views/dashboard/TeamsView.vue";
+import Dashboard from "@/views/DashboardView.vue";
+import Fixtures from "@/views/public/home/FixturesView.vue";
+import MatchView from "@/views/public/home/MatchView.vue";
+import Players from "@/views/public/home/PlayersView.vue";
+import Stats from "@/views/public/home/StatsView.vue";
+import Table from "@/views/public/home/TableView.vue";
+import Home from "@/views/public/HomeView.vue";
 
 const routes = [
   {
@@ -82,7 +82,7 @@ const routes = [
       { path: "/stats", name: "Stats", component: Stats },
       { path: "/players", name: "Players", component: Players },
       { path: "/matches", name: "Matches", component: MatchesView },
-      { path: "/match/:id", name: "Match", component: MatchView },
+      { path: "/match/:id", name: "MatchDetails", component: MatchView },
     ],
   },
   {
--- src/views/public/HomeView.vue
+++ src/views/public/HomeView.vue
@@ -2,6 +2,11 @@
   import SubnavbarMoleculeVue from "@/components/molecules/SubnavbarMolecule.vue";
   import NavbarMolecule from "@/components/molecules/NavbarMolecule.vue";
   import { ILink } from "~/types/global";
+  import { onBeforeRouteUpdate, useRoute } from "vue-router";
+  import { ref } from "vue";
+  import router from "@/router";
+
+  const route = useRoute();
 
   const links: ILink[] = [
     { name: "Fixtures", href: "/" },
@@ -14,6 +19,18 @@
     { name: "Home", href: "/" },
     { name: "Bet", href: "/bet" },
   ];
+  const routesWithoutMenu = ["MatchDetails"];
+  const showMenu = ref(true);
+
+  onBeforeRouteUpdate((to) => {
+    console.log(
+      "before update",
+      routesWithoutMenu.includes(to.name.toString()),
+      to.name.toString(),
+    );
+    if (to.name)
+      showMenu.value = !routesWithoutMenu.includes(to.name.toString());
+  });
 </script>
 
 <template>
@@ -22,7 +39,7 @@
       <NavbarMolecule :links="homeLinks" />
     </div>
     <div class="home--page__wrapper px-1 md:px-20">
-      <SubnavbarMoleculeVue :links="links">
+      <SubnavbarMoleculeVue v-if="showMenu" :links="links">
         <div class="pb-14 pt-8">
           <h4 class="">Football Competition</h4>
           <h1 class="text-2xl font-bold pt-2">Interclass league</h1>
