1e55df309c011491a931d3bdbcbaf149c85c19e4
claranceliberi <liberintwari@gmail.com>
2022-03-07 23:30:22 +0200

    fortfait

--- src/components/matchday/MatchesInMatchDay.vue
+++ src/components/matchday/MatchesInMatchDay.vue
@@ -7,6 +7,7 @@
       >
         <router-link :to="props.url.replace(/:[\w]+/gi, match.id)">
           <SingleMatch
+            :forfeit="match.forfeit"
             :match="match.id"
             :home-team="match.home_team.name"
             :away-team="match.away_team.name"
