18e8f58c6bf124c690cc6d4e0074a2934f805cc9
Jacques Twizeyimana <51080275+jacques-twizeyimana@users.noreply.github.com>
2022-04-12 12:13:55 +0200

    feat: use random player avatar

--- src/components/molecules/PlayerCard.vue
+++ src/components/molecules/PlayerCard.vue
@@ -4,7 +4,7 @@
       :src="
         imageUrl
           ? imageUrl
-          : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+          : 'https://i.pravatar.cc/200'
       "
       class=""
       alt=""
